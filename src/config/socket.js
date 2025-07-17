3752342d3a9179fcf3b3d16599314fe61e12fe5d
tuan-hda <anhtuan.hoangdinh@gmail.com>
2025-07-17 15:41:14 +0700

    Fixed accessing non exist value

--- src/config/socket.js
+++ src/config/socket.js
@@ -1,5 +1,5 @@
 // Copyright (c) 2025 Eclipse Foundation.
-// 
+//
 // This program and the accompanying materials are made available under the
 // terms of the MIT License which is available at
 // https://opensource.org/licenses/MIT.
@@ -22,7 +22,7 @@ let io = null;
 const init = (server) => {
   io = new Server(server, {
     cors: {
-      origin: config.cors.regex,
+      origin: '*',
       credentials: true,
     },
   });
